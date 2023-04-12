a7f77ce8366d8f6c621d14587f0c29e4163f6972
Farai Zihove <farai@flickerleap.com>
2023-04-12 10:32:29 +0200

    updated migration

--- src/migrations/0000_00_00_000000_create_clickatell_tracking_table.php
+++ src/migrations/0000_00_00_000000_create_clickatell_tracking_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClickatellTrackingTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -35,4 +35,4 @@ class CreateClickatellTrackingTable extends Migration
     {
         Schema::dropIfExists(config('services.clickatell.tracking_table'));
     }
-}
+};
